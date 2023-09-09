5edc891b47c06c94d73c67b621b5b180c61cf5a9
krone-git <kronebotprojects@gmail.com>
2023-09-09 18:14:43 -0500

    Separated general plotter testing.

--- test.py
+++ test.py
@@ -1,35 +1,9 @@
-import tkinter as tk
-from math import sin
-
-from tensor import Tensor
+from spline import ControlArm
 from vector import Vector
-from matrix import Matrix
-from plot import TkinterGraphPlotterCanvas, TkinterInterpolationCurve
 
 
-app = tk.Tk()
+v = Vector.fill(3, 1)
+arm = ControlArm.fill(3, 1)
 
-canvas = TkinterGraphPlotterCanvas(
-    app, resolution=50
-)
-canvas.pack(expand=tk.YES, fill=tk.BOTH)
-canvas.origin.set_origin(
-    canvas.winfo_reqwidth() / 2, canvas.winfo_reqheight() / 2,
-)
-canvas.update()
-
-function = lambda x: (10 * x, 10 * sin(x))
-curve = TkinterInterpolationCurve(
-    canvas,
-    canvas.origin,
-    True,
-    function,
-    1,
-    "black",
-    "black",
-    4
-)
-curve.update()
-canvas._curves.append(curve)
-
-app.mainloop()
+v.print()
+arm.print()
